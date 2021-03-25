815817faecfb4a6bc021b05cd7a26c24f1d2f2da
Kyle <kyleferry@icloud.com>
2021-03-25 06:53:42 +0000

    Header desktop design done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import logo from '../../assets/images/kf_logo_Blk.png'
+
+import logo from '../../assets/images/kf_logo.png'
 import Twitter from '@material-ui/icons/Twitter';
 import LinkedIn from '@material-ui/icons/LinkedIn';
 import GitHub from '@material-ui/icons/GitHub';
+import MenuIcon from '@material-ui/icons/Menu'
+
+const headerTabs = [
+  {
+    label: 'Home',
+    href: '#'
+  },
+  {
+    label: 'About',
+    href: '#'
+  },
+  {
+    label: 'Portfolio',
+    href: '#'
+  },
+  {
+    label: 'Contact',
+    href: '#'
+  },
+]
+
+
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    backgroundColor: '#fff'
-  },
-  logo: {
-    maxHeight: 50,
-    paddingRight: 25,
+    backgroundColor: '#000000'
   },
   logo: {
     maxHeight: 50,
@@ -22,37 +40,119 @@ const useStyles = makeStyles((theme) => ({
   },
   icon: {
     marginRight: 25,
-  }
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+  },
+  sociolTabs: {
+    color: '#fff',
+    marginLeft: "38px",
+  },
+  Toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
 }));
 
 const Header = () => {
 
+  const [state, setState] = useState({
+    mobileView: false
+  })
+
+  const { mobileView } = state
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }))
+    };
+    setResponsiveness();
+    window.addEventListener('resize', () => setResponsiveness())
+  }, [])
+
   const classes = useStyles();
+
+  const displayDesktop = () => {
+    return <Toolbar className={classes.Toolbar}>
+      <div>
+        {kfLogo}
+        {getHeaderTabs()}
+      </div>
+      {socialLinks}
+    </Toolbar>
+  }
+
+  const displayMobile = () => {
+    return (
+      <Toolbar>
+      <div>{kfLogo}</div>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        
+      </Toolbar>
+    )
+  }
+
+  const kfLogo = (
+    <Button>
+      <img src={logo} alt="Kyle Ferry logo" className={classes.logo} />
+    </Button>
+  )
+
+  const getHeaderTabs = () => {
+    return headerTabs.map(({ label, href }) => {
+      return (
+        <Button {...{
+          key: label,
+          color: 'inherit',
+          to: href,
+          className: classes.menuButton
+        }}>
+          {label}
+        </Button>
+      )
+    })
+  }
+
+  const socialLinks = (
+    <div>
+      <IconButton href='https://github.com/kcferry'>
+        <GitHub className='icon' color="primary" />
+      </IconButton>
+
+      <IconButton href='https://www.linkedin.com/in/kyle-ferry-2bb160112'>
+        <LinkedIn className='icon' color="primary" />
+      </IconButton>
+
+      <IconButton href='https://twitter.com/Kvferry'>
+        <Twitter className='icon' color="primary" />
+      </IconButton>
+    </div>
+  )
+
+
+
 
   return (
     <>
       <AppBar className={classes.appBar} position="static" >
-        <Toolbar>
-          <img src={logo} alt="logo" className={classes.logo} />
-
-          <Button href="#text-buttons" color="primary">Home</Button>
-          <Button href="#text-buttons" color="primary">About</Button>
-          <Button href="#text-buttons" color="primary">Portfolio</Button>
-          <Button href="#text-buttons" color="primary">Contact</Button>
-
-          <a href="https://github.com/kcferry">
-            <GitHub className='icon' color="primary" style={{ marginLeft: 980 }} />
-          </a>
-
-          <a href="https://www.linkedin.com/in/kyle-ferry-2bb160112/">
-            <LinkedIn className='icon' color="primary" style={{ marginLeft: 30 }} />
-          </a>
-
-          <a href="https://twitter.com/Kvferry">
-            <Twitter className='icon' color="primary" style={{ marginLeft: 30 }} />
-          </a>
-
-        </Toolbar>
+        {mobileView ? displayMobile() : displayDesktop()}
       </AppBar>
     </>
   )
--- src/Hero.js
+++ src/Hero.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import mainImage from './assets/images/dl-main.jpg'
+import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
     hero: {
         backgroundImage: `url(${mainImage})`,
-        height: '900px',
+        height: 'calc(100vh - 32px)',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-reapeat',
         backgroundSize: 'cover',
@@ -17,10 +18,8 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "center",
         color: '#fff',
         fontSize: "4rem"
-
     }
-
-  }));
+}));
 
 const Hero = () => {
 
@@ -29,7 +28,20 @@ const Hero = () => {
     return (
         <>
             <Box className={classes.hero}>
-                <h2>Hi, I'm Kyle Ferry</h2>
+                
+                <Typography variant="h6" component="h2" gutterBottom>
+                    WELCOME
+                </Typography>
+                <Typography variant="h4" gutterBottom>
+                    <h1>Hi, I'm Kyle Ferry</h1>
+                </Typography>
+                <Typography variant="h4" gutterBottom>
+                    <h1>Full Stack Developer</h1>
+                </Typography>
+                <Typography variant="h4" gutterBottom>
+                    <h1>Based in London</h1>
+                </Typography>
+
             </Box>
         </>
     )
