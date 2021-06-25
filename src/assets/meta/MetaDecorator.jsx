699d93e658824c28fd6b8d897038dc83c810091c
Kyle <kyleferry@icloud.com>
2021-06-25 04:05:04 +0100

    Added dynamic OG meta component bugFix 3

--- src/assets/meta/MetaDecorator.jsx
+++ src/assets/meta/MetaDecorator.jsx
@@ -8,13 +8,13 @@ const MetaDecorator = ({ image }) => {
 
     return (
         <Helmet>
-            <title>Kyle Ferry</title>
-            <meta name="description" content="Website containing web development projects created by Kyle Ferry" />
-            <meta property="og:title" content="Kyle Ferry Developer Portfolio" />
-            <meta property="og:url" content={metaData.hostname + window.location.pathname + window.location.search} />
-            <meta property="og:description" content="Website containing web development projects created by Kyle Ferry." />
-            <meta property="og:type" content='website' />
-            <meta property="og:image" content={metaData.hostname + image} />
+            <title>Kyle Ferry Portfolio</title>
+            <meta name="description" content="Website containing web development projects created by Kyle Ferry"> </meta>
+            <meta property="og:title" content="Kyle Ferry Developer Portfolio"> </meta>
+            <meta property="og:url" content={metaData.hostname}> </meta>
+            <meta property="og:description" content="Website containing web development projects created by Kyle Ferry."> </meta>
+            <meta property="og:type" content='website'> </meta>
+            <meta property="og:image" content={metaData.hostname + image}> </meta>
         </Helmet>
     )
 }
